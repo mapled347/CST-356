0d451c363e371a502dc2d98c5a809e68cf0e44d2
mapled347 <46737168+mapled347@users.noreply.github.com>
2019-03-11 22:38:09 -0700

    Week 7 initial start

---
+++ Week7/webapi/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Database;
+using Microsoft.AspNetCore.Authorization;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class StudentsController : ControllerBase
+    {
+        
+        private readonly SchoolContext _dbContext;
+
+        public StudentsController(SchoolContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(_dbContext.Student.Include(p => p.Person).ToList());
+        }
+
+        // [HttpGet("{studentId}")]
+        // public ActionResult<Student> GetStudent(int studentId)
+        // {
+        //     var student = _dbContext.Student
+        //         .SingleOrDefault(p => p.StudentId == studentId);
+
+        //     if (student != null) {
+        //         return student;
+        //     } else {
+        //         return NotFound();
+        //     }
+        // }
+
+
+    }
+}
---
+++ Week7/webapp/src/components/Persons.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h2 class='section-heading'>Persons</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>MI</th>
+                    <th>Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in persons" :key="person.id">
+                    <td>{{ person.firstName }}</td>
+                    <td>{{ person.middleInitial }}</td>
+                    <td>{{ person.lastName }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    export default {
+        name: 'Persons',
+        
+        data () {
+            return {
+                persons: []
+            }
+        },
+        methods: {
+            getPersons: function() {
+                let personsApi = process.env.PERSON_API;
+                Vue.axios.get(personsApi).then(
+                    (response) => {
+                        console.log(response)
+                        this.persons = response.data;
+                    },
+                    (error) => {
+                        console.log(error)
+                    }
+                );  
+            }
+        },
+        mounted() {
+            this.getPersons();
+        }
+    }
+</script>
+
+<style scoped>
+    table {
+        border-collapse: collapse;
+    }
+    table, th, td {
+        border: 1px solid black;
+        padding: 10px;
+    }
+</style>
